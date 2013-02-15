9c26ff7dd2d07414c4a4c693da0648c3689b439c
mattadam <mattnery@yahoo.com>
2013-02-15 08:50:16 -0700

    added scraping for all relevant data

--- README.md
+++ README.md
@@ -3,4 +3,6 @@ Example of screen scraping using Nokigiri
 
 TO DO:
 1. Write Rake task
-2. Include all relevant data
+2. Test for invalid parameters
+3. Handle invalid parameters
+4. Write methods for undervalued overvalued and neutral
--- nokogiri_test.rb
+++ nokogiri_test.rb
@@ -8,14 +8,30 @@ require 'open-uri'
 def stock_retrieve(stock)
 
 url = "http://finviz.com/quote.ashx?t="+ stock
+  puts url
 doc = Nokogiri::HTML(open(url))
   ticker = doc.at_css(".fullview-ticker").text
-  price = doc.at_css(":nth-child(10) :nth-child(12) b").text[/[0-9\.]+/]
-  price_to_book = doc.at_css(":nth-child(5) :nth-child(4) b").text[/[0-9\.]+/]
-
-  puts "#{ticker} - #{price} price to book - #{price_to_book}"
+  price = doc.at_css(":nth-child(11) :nth-child(12)").text
+  price_to_book = doc.at_css(":nth-child(5) :nth-child(4)").text
+  debt_to_equity = doc.at_css(":nth-child(10) :nth-child(4)").text
+  eps_this_year = doc.at_css(":nth-child(4) .snapshot-td2:nth-child(6) span").text
+  eps_next_year = doc.at_css(":nth-child(5) :nth-child(6)").text
+  current_ratio = doc.at_css(":nth-child(9) :nth-child(4)").text
+  insider_trans = doc.at_css(".table-dark-row:nth-child(2) :nth-child(8) b").text
+  gross_margin = doc.at_css(":nth-child(8) :nth-child(8)").text
+  
+  puts "#{ticker} #{price}"
+  puts "P/B = #{price_to_book}"
+  puts "D/E = #{debt_to_equity}"
+  puts "EPS this year = #{eps_this_year}"
+  puts "EPS next year = #{eps_next_year}"
+  puts "Current Ratio = #{current_ratio}"
+  puts "Insider transaction = #{insider_trans}"
+  puts "Gross Margin = #{gross_margin}"
+  
 end
 
 stock_retrieve('goog')
 stock_retrieve('aapl')
 stock_retrieve('amzn')
+
